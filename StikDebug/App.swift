34cfbc143f0114893b5441a0b1bdead0eb7a5c92
colin0252 <zkn12306@outlook.com>
2026-07-01 22:25:53 +0800

    更新 App.swift

--- StikDebug/App.swift
+++ StikDebug/App.swift
@@ -1,19 +1,10 @@
 import SwiftUI
-import UIKit
 
-@main   // 这个标记告诉系统，这是应用的入口
+@main
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
-            ViewControllerWrapper()
+            DeltaTokenMainView()
         }
     }
-}
-
-// 把你的 ViewController 包装成 SwiftUI 能用的形式
-struct ViewControllerWrapper: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> ViewController {
-        return ViewController()
-    }
-    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
 }
