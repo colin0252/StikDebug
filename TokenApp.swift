a078cd47699b74252088fcd9ade6f4f00919b63d
colin0252 <zkn12306@outlook.com>
2026-07-04 17:24:45 +0800

    创建 TokenApp.swift

---
+++ TokenApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct TokenHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .ignoresSafeArea()
+                .preferredColorScheme(.light)
+        }
+    }
+}
